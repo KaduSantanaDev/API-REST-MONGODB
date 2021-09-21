e262c1d9981f5cf38f2adc0f6385ddd63190f9b4
kadu <kaduzera.ks@gmail.com>
2021-09-20 21:25:17 -0300

    configurando o mongoose

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./src/routes');
 
 const app = express();
-
+mongoose.connect(process.env.CONNENCTIONSTRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('conectei');
+  app.emit('event');
+});
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.listen(3003, () => {
-  console.log('http://localhost:3003');
+app.on('event', () => {
+  app.listen(3003, () => {
+    console.log('http://localhost:3003');
+  });
 });
